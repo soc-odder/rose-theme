8daec998504efb524ee46817c16d7fbedca118b4
Lars Olesen <lars@intraface.dk>
2013-06-18 21:41:50 +0200

    Removed uneccessary code

--- templates/comment.tpl.php
+++ templates/comment.tpl.php
@@ -73,8 +73,6 @@
 
   <div class="content"<?php print $content_attributes; ?>>
     <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['links']);
       print render($content);
     ?>
     <?php if ($signature): ?>
